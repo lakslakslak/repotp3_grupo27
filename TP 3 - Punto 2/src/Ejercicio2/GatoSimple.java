b9c99ed661b2a19b09b4b5bf1f8f5e2342c69f72
Rioal <Rioal@AlstKill06>
2024-09-15 00:11:48 -0300

    Ejercicio 2

---
+++ TP 3 - Punto 2/src/Ejercicio2/GatoSimple.java
@@ -0,0 +1,75 @@
+package Ejercicio2;
+
+public class GatoSimple implements Comparable<GatoSimple> {
+    // Atributos privados
+    private String color;
+    private double peso;
+    private String raza;
+    private int edad;
+    private String nombre;
+    private String sexo;  // macho o hembra
+
+    // Constructor parametrizado
+    public GatoSimple(String color, double peso, String raza, int edad, String nombre, String sexo) {
+        this.color = color;
+        this.peso = peso;
+        this.raza = raza;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.sexo = sexo;
+    }
+
+    // Método maullar
+    public void maullar() {
+        System.out.println("Miauu");
+    }
+
+    // Método ronronear
+    public void ronronear() {
+        System.out.println("prrrr");
+    }
+
+    // Método comer
+    public void comer(String comida) {
+        if (comida.equalsIgnoreCase("pescado")) {
+            System.out.println("Que rico ¡Gracias!!");
+        } else {
+            System.out.println("Lo siento, yo solo como pescado");
+        }
+    }
+
+    // Método pelear
+    public void pelear(GatoSimple gatoContrincante) {
+        if (this.sexo.equalsIgnoreCase("hembra")) {
+            System.out.println("No me gusta pelear");
+        } else if (this.sexo.equalsIgnoreCase("macho")) {
+            if (gatoContrincante.getSexo().equalsIgnoreCase("hembra")) {
+                System.out.println("No peleo contra gatitas");
+            } else {
+                System.out.println("¡Ven aquí que te vas a enterar!");
+            }
+        }
+    }
+
+    // Métodos getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    // Para la comparación en el TreeSet
+    @Override
+    public int compareTo(GatoSimple otroGato) {
+        return this.nombre.compareTo(otroGato.nombre);
+    }
+
+    // Sobreescribir toString para mostrar los atributos
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Sexo: " + sexo + ", Color: " + color + ", Peso: " + peso + 
+               ", Raza: " + raza + ", Edad: " + edad;
+    }
+}
---
+++ TP 3 - Punto 2/src/module-info.java
@@ -0,0 +1,2 @@
+module STS {
+}
